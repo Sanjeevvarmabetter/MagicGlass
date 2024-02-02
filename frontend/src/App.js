ccb38eaa1d03caa80ae620e915bb517c0c513e38
uttejterlapu <uterlapu@gitam.in>
2024-02-02 15:13:18 +0530

    fixed routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,16 @@
 import './App.css';
 import React from 'react';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Route, BrowserRouter, Routes,Router } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-
 
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Routes>
+        <Router>
           <Route path='/' element={Home} />
           <Route path='/' element={About} />
-        </Routes>
-      </BrowserRouter>
+        </Router>
     </div>
   );
 }
