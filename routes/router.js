6da174c8cf556a4557077ab50915123d37a72475
uttejterlapu <uterlapu@gitam.in>
2024-02-03 16:38:36 +0530

    revert

---
+++ routes/router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const Registration = require('../models/registrationform');
+
+router.post('/registration', async (req, res) => {
+    try {
+        const {username, email, country, address, phone, eventCategory, expertise } = req.body;
+
+        const newRegistration = new Registration({
+            username: username,
+            email: email,
+            country: country,
+            address: address,
+            phone: phone,
+            eventCategory: eventCategory,
+            expertise: expertise
+        });
+
+        await newRegistration.save();
+
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        console.error('Error registering user', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+module.exports = router;
---
+++ testserver.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const router = require('./routes/router')
+const mongoose = require('mongoose');
+const app = express()
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:false}))
+
+const corseOptions = {
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200
+}
+
+const dbOptiosns = {useNewUrlParser:true, useUnifiedTopology:true}
+mongoose.connect('mongodb+srv://shore:shore@cluster0.uubfvtd.mongodb.net/registration',dbOptiosns)
+.then(() => console.log("DB connected!"))
+.catch(err => console.log(err))
+
+app.use(cors(corseOptions))
+app.use('/', router)
+
+const PORT = 5000
+
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
