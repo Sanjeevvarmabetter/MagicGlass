597cdc998ddcfe165ea16edd67f6cb41168b474d
uttejterlapu <uterlapu@gitam.in>
2024-02-04 07:55:12 +0530

    add more locations on map

--- shore/src/pages/Components/Map.js
+++ shore/src/pages/Components/Map.js
@@ -20,6 +20,30 @@ const SimpleMap = () => {
   const markerLocations = [
     { position: [17.6868, 83.2185], content: "valorant tournment available" },
     { position: [17.686, 82.219], content: "cricket competition" },
+    { position: [42.546245, 42.546245], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [23.424076, 23.424076], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [33.93911, 33.93911], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [17.060816, 17.060816], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [18.220554, 18.220554], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [41.153332, 41.153332], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [40.069099, 40.069099], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [12.226079, 12.226079], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [-11.202692, -11.202692], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [-75.250973, -75.250973], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [-38.416097, -38.416097], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [-14.270972, -14.270972], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [47.516231, 47.516231], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [-25.274398, -25.274398], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [12.52111, 12.52111], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [40.143105, 40.143105], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [43.915886, 43.915886], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [13.193887, 13.193887], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [23.684994, 23.684994], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [50.503887, 50.503887], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [12.238333, 12.238333], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [42.733883, 42.733883], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    { position: [25.930414, 25.930414], content: ' QWERTYUIOPASDFGHJKLZXCVBNM ' },
+    
     // Add more locations as needed
   ];
 
